cad5d334546a7561272bbccff751f26116b9ecbb
pl576 <106371659+pl576@users.noreply.github.com>
2022-06-02 01:19:46 +0200

    update all

---
+++ CV.js
@@ -0,0 +1,45 @@
+const slidingLoader = document.querySelector(".loader");
+const nameAnim = document.querySelector(".name");
+const shortDescriptionAnim = document.querySelector(".short_description");
+let a = document.getElementsByTagName("body");
+
+function slide() {
+  slidingLoader.classList.add("active");
+  nameAnim.classList.add("active");
+  shortDescriptionAnim.classList.add("active");
+
+  for (valeur of a) {
+    valeur.style.overflowY = "scroll";
+  }
+}
+
+const slidingSkillsLeft1 = document.querySelector(".left1");
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight } = document.documentElement;
+
+  const topElementToTopViewport =
+    slidingSkillsLeft1.getBoundingClientRect().top;
+
+  if (
+    scrollTop >
+    (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.5
+  ) {
+    slidingSkillsLeft1.classList.add("active");
+  }
+});
+const slidingSkillsLeft2 = document.querySelector(".left2");
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight } = document.documentElement;
+
+  const topElementToTopViewport =
+    slidingSkillsLeft2.getBoundingClientRect().top;
+
+  if (
+    scrollTop >
+    (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.5
+  ) {
+    slidingSkillsLeft2.classList.add("active");
+  }
+});
